43054763fecad5aa75d49fca62155894ca43493e
不知火 <supersonic@livemail.tw>
2026-01-27 10:00:10 +0800

    fix(config): update discord server url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,7 @@ export default defineNuxtConfig({
       hfOrgName: 'twinkle-ai',
     },
     externalUrls: {
-      discord: 'https://discord.gg/Cx737yw4ed',
+      discord: 'https://discord.com/servers/twinkle-ai-1310544431983759450',
       github: 'https://github.com/ai-twinkle',
       huggingface: 'https://huggingface.co/twinkle-ai',
     },
